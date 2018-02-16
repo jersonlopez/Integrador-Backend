2aa822ec20b9371cd34b7e9e565f9ae7a1424aa3
sergiocastri <scastrillon347@gmail.com>
2018-02-16 12:20:03 -0500

    solution bug

--- App/features/db/loan/util.js
+++ App/features/db/loan/util.js
@@ -4,13 +4,14 @@ let modelDevolution = require('../devolution/model') //revisar ruta
 devolution = modelDevolution.getDevolution()
 loan = modelLoan.getLoan()
 
-function getSanction(req, res) {  //getDevolution?&&id=123456   METODO get devuelve un json con la fecha del ultimo prestamo (implemento a devolver) tipo de implemento id.
+function getSanction(req, res) {  //localhost:3000/v1/getDevolution/?id=222   METODO get devuelve un json con la fecha del ultimo prestamo (implemento a devolver) tipo de implemento id.
   let sanctionTime;
   devolution.find({id: req.query.id}, '-_id -__v -attendant -typeImplement -observation', function(err, doc) {                                               
       if(doc.length > 0) {     
         if(parseInt(doc[doc.length - 1].timeSanction) > 0){
           sanctionTime = Math.floor((parseInt(doc[doc.length - 1].timeSanction)/86400000) +1);
           res.send("USUARIO SANCIONADO; aun tiene " + sanctionTime + " Dias de sancion");
+          return;
         }                                                                                                                         
       }
       res.send("Usuario NO sancionado");
