95bb3b9a3d34b10dcdaa453a2aeddf7149375d6a
jersonlopez <jersonlopez.1204@gmail.com>
2018-02-19 15:58:09 +0000

    consumption MARES services full

--- App/features/consumptionMares/consumption.js
+++ App/features/consumptionMares/consumption.js
@@ -1,17 +1,19 @@
 let java = require("java");
+let path = require("path")
 
+let root = path.resolve(__dirname)
 
-java.classpath.push("/libs_java/log4j-1.2.9.jar");
-java.classpath.push("/libs_java/commons.collections-3.2.1.jar");
-java.classpath.push("/libs_java/commons-beanutils-1.7.0.jar");
-java.classpath.push("/libs_java/commons-lang-2.4.jar");
-java.classpath.push("/libs_java/commons-logging-1.1.1.jar");
-java.classpath.push("/libs_java/ezmorph-1.0.6.jar");
-java.classpath.push("/libs_java/jersey-client-1.2.jar");
-java.classpath.push("/libs_java/jersey-core-1.2.jar");
-java.classpath.push("/libs_java/json-lib-2.3-jdk13.jar");
-java.classpath.push("/libs_java/jsr311-api-1.1.1.jar");
-java.classpath.push("/libs_java/OrgSistemasSecurity.jar");
+java.classpath.push(root + "/libs_java/log4j-1.2.9.jar");
+java.classpath.push(root + "/libs_java/commons.collections-3.2.1.jar");
+java.classpath.push(root + "/libs_java/commons-beanutils-1.7.0.jar");
+java.classpath.push(root + "/libs_java/commons-lang-2.4.jar");
+java.classpath.push(root + "/libs_java/commons-logging-1.1.1.jar");
+java.classpath.push(root + "/libs_java/ezmorph-1.0.6.jar");
+java.classpath.push(root + "/libs_java/jersey-client-1.2.jar");
+java.classpath.push(root + "/libs_java/jersey-core-1.2.jar");
+java.classpath.push(root + "/libs_java/json-lib-2.3-jdk13.jar");
+java.classpath.push(root + "/libs_java/jsr311-api-1.1.1.jar");
+java.classpath.push(root + "/libs_java/OrgSistemasSecurity.jar");
 
 /*module.exports = {
   index: function(req, res) {
