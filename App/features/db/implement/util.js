cc483540a7b893f40ee568d66f3da78869c566c7
sergiocastri <scastrillon347@gmail.com>
2018-02-20 17:46:22 -0500

    update implement

--- App/features/db/implement/util.js
+++ App/features/db/implement/util.js
@@ -3,13 +3,29 @@ let model = require('./model')
 implement = model.getImplements()
 
 function saveImplements (req, res) { // función para guardar implemento
-  let newImplement = new implement({
-    typeImplement: req.body.typeImplement, quantity: req.body.quantity
-  })
 
-  newImplement.save(function () {
-    res.send("Implemento registrado exitosamente" /*+ newImplement*/)
-  })
+  implement.find({}, '-_id -__v', function (err, doc) {   //// PROBARRRRRR
+    let i = 0;
+    for (i; i < doc.length; i++) {
+      if (req.body.typeImplement == doc[i].typeImplement) {
+        doc[i].quantity = doc[i].quantity + req.body.quantity;
+        implement.findOne({ typeImplement: req.params.typeImplement }, function (err, implement) {
+          if (err) return handleError(err);
+          implement.quantity = doc[i].quantity;
+          implement.save(function () {
+            res.send("Actualizamos tus datos");
+          });
+        });
+      } else {
+        let newImplement = new implement({
+          typeImplement: req.body.typeImplement, quantity: req.body.quantity
+        });
+        newImplement.save(function () {
+          res.send("Implemento registrado exitosamente" /*+ newImplement*/)
+        })
+      }
+    }
+  });
 };
 
 
