bec9dcc822f41bf164dca68bb24f8f9c99245cb7
sergiocastri <scastrillon347@gmail.com>
2018-02-21 13:18:25 -0500

    Update correct

--- App/features/db/implement/util.js
+++ App/features/db/implement/util.js
@@ -2,31 +2,23 @@ let model = require('./model')
 
 implement = model.getImplements()
 
-function saveImplements (req, res) { // función para guardar implemento
-
-  implement.find({}, '-_id -__v', function (err, doc) {   //// PROBARRRRRR
-    let i = 0;
-    for (i; i < doc.length; i++) {
-      if (req.body.typeImplement == doc[i].typeImplement) {
-        doc[i].quantity = doc[i].quantity + req.body.quantity;
-        implement.findOne({ typeImplement: req.params.typeImplement }, function (err, implement) {
-          if (err) return handleError(err);
-          implement.quantity = doc[i].quantity;
-          implement.save(function () {
-            res.send("Actualizamos tus datos");
-          });
-        });
-      } else {
-        let newImplement = new implement({
-          typeImplement: req.body.typeImplement, quantity: req.body.quantity
-        });
-        newImplement.save(function () {
-          res.send("Implemento registrado exitosamente" /*+ newImplement*/)
-        })
-      }
-    }
+function saveImplements(req, res) { // función para guardar implemento
+  implement.find( {typeImplement: req.body.typeImplement}, '-_id -__v', function (err, doc) {   //// PROBARRRRRR
+    let updateQuantity;
+    let oldQuantity;
+    oldQuantity = doc[0].quantity;
+    updateQuantity = doc[0].quantity + req.body.quantity;
+    let Implement = new implement({
+      typeImplement : req.body.typeImplement, quantity : updateQuantity
+    })
+    Implement.save(function () {
+      res.send("Actualizada cantidad de " + req.body.typeImplement /*+ newImplement*/)
+    })
+    implement.findOneAndRemove({typeImplement:req.body.typeImplement, quantity: oldQuantity}, function(err) {
+    });  
   });
 };
+
 
 
 function getAllImplements (req, res) {
